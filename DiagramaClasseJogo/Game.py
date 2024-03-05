89a78f88b887c2534719132d887168d58baaefa3
Gilmarcos <almeida.gilmarcos@gmail.com>
2024-03-05 05:39:24 -0300

    Signed-off-by: Gilmarcos <almeida.gilmarcos@gmail.com>

---
+++ DiagramaClasseJogo/Game.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Game:
+    def __init__(self):
+        self.window = None
+        self.Attribute1 = None
+
+    def run(self, ):
+        pass
+
